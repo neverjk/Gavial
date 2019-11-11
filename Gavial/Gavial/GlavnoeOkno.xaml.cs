21fd03b47d6530c03a45a4ee0e8593d43fcdbee6
pigeon <pigeon@A12PC7>
2019-11-11 20:57:05 +0200

    added colorpicker

--- Gavial/Gavial/GlavnoeOkno.xaml.cs
+++ Gavial/Gavial/GlavnoeOkno.xaml.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,7 +24,44 @@ namespace Gavial
     {
         public GlavnoeOkno()
         {
+            var color_query =
+        from PropertyInfo property in typeof(Colors).GetProperties()
+        orderby property.Name
+                 //orderby ((Color)property.GetValue(null, null)).ToString()
+                 select new ColorInfo(
+            property.Name,
+            (Color)property.GetValue(null, null));
+
             InitializeComponent();
+            ColorCB.ItemsSource = color_query;
+
+
+        }
+        public static int Fibonacci(int n)
+        {
+            int a = 0;
+            int b = 1;
+            // In N steps compute Fibonacci sequence iteratively.
+            for (int i = 0; i < n; i++)
+            {
+                int temp = a;
+                a = b;
+                b = temp + b;
+            }
+            return a;
+        }
+
+
+        private ComboBox FillSize()
+        {
+            ComboBox box;
+            for (int i = 0; i < 15; i++)
+            {
+                Console.WriteLine(Fibonacci(i));
+            }
+            //box.Items.Add();
+    
+            return null;
         }
     }
 }
